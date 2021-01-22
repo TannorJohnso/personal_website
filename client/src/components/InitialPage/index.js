262bc0bdf87d0f82cc7e3b2bfafdea4949888d15
Tannor Johnson <johnson.tannor@gmail.com>
2021-01-22 15:14:05 -0500

    Creates svg box around name

--- client/src/components/InitialPage/index.js
+++ client/src/components/InitialPage/index.js
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import arrowDown from '../../assets/downArrow.png';
 import frisbee from '../../assets/frisbee.png';
 import { Animate } from 'react-simple-animate';
+import { useSpring, animated } from 'react-spring';
+import { Spring } from 'react-spring/renderprops';
+
 import './styles.css';
 
 const InitialPage = () => {
   const [play, setPlay] = useState(false);
 
+  useEffect(() => {
+    setPlay(!play);
+  }, []);
+
   return (
     <div className="initial_page">
-      <div className="initPageName">
-        <h2>Hello, I'm</h2>
-        <h1>Tannor Johnson</h1>
-        <p>I'm a Web Developer.</p>
-        <Animate
-          play={play} // Toggle when animation should start
-          start={{
-            transform: 'translateX(-100px) rotateZ(0)',
-          }}
-          end={{ transform: 'translateX(200px) rotateZ(720deg)' }}
-        >
-          <img alt="frisbee" src={frisbee} className="frisbee" />
-        </Animate>
-        <button onClick={() => setPlay(!play)}>Play</button>
-      </div>
+      <Animate
+        play={play}
+        start={{ opacity: 0, filter: 'blur(10px)' }}
+        end={{ opacity: 1, filter: 'blur(0)', height: '100%' }}
+      >
+        <div className="initPageName">
+          <h2>Hello, I'm</h2>
+          <h1>Tannor Johnson</h1>
+          <p>I'm a Web Developer.</p>
+          <svg viewBox="0 0 100 100" class="svgBox">
+            <path
+              class="path"
+              fill="white"
+              stroke="black"
+              stroke-width="2"
+              d="M 75,50 v 25 h -50 v -50 h 50 Z"
+            />
+          </svg>
+        </div>
+      </Animate>
       <button
         onClick={() => {
           document.querySelector('#about').scrollIntoView({
@@ -39,3 +51,42 @@ const InitialPage = () => {
 };
 
 export default InitialPage;
+
+{
+  /* <Animate
+        play={play} // Toggle when animation should start
+        duration={3}
+        start={{
+          transform: 'translateX(0px) translateY(0)',
+        }}
+        end={{
+          transform: 'translateX(84%) translateY(100%)',
+        }}
+      >
+        <img alt="frisbee" src={frisbee} className="frisbee" />
+      </Animate> */
+}
+
+{
+  /* <svg>
+            <path
+              stroke="black"
+              d="M 10,30
+       A 20,20 0,0,1 50,30
+       A 20,20 0,0,1 90,30
+       Q 90,60 50,90
+       Q 10,60 10,30 z"
+            />
+            <animated.path
+              fill="none"
+              stroke="red"
+              strokeDasharray="500"
+              strokeDashoffset="0"
+              d="M 10,30
+       A 20,20 0,0,1 50,30
+       A 20,20 0,0,1 90,30
+       Q 90,60 50,90
+       Q 10,60 10,30 z"
+            />
+          </svg> */
+}
